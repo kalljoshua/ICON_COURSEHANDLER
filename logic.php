72cbf0488423121b3be48f5f2afc58a818106eec
kalljoshua <nabukajoshua@gmail.com>
2016-07-12 14:02:21 +0300

    Form_Logic

--- logic.php
+++ logic.php
@@ -2,9 +2,25 @@
 <form action="logics.php" method="GET">
      <div id="dynamicInput">
 					<label>Select</label>
-                    <select class="form-control" name="idcollege" Required="true">
-                        <option value="math">Math</option>
-					</select>
+                    <select class="form-control" name="idsubject" Required="true">
+                        <option>Select College</option>
+						<?php 
+						include('db_connect.php');
+							$query_college = mysqli_query($con,'SELECT * FROM subject');
+							if(mysqli_num_rows($query_college)>0){
+								while($rows = mysqli_fetch_array($query_college)){
+									$idsubject = $rows['idsubject'];
+									$name = $rows['name'];
+									
+							?>
+							<option value='<?php echo $name; ?>'><?php echo $name; ?></option>
+							<?php
+								}
+							
+							}
+						?>
+                        
+                      </select>
          
      </div>
      <input type="button" value="Add another text input" onClick="addInput('dynamicInput');">
@@ -20,7 +36,7 @@ function addInput(divName){
      }
      else {
           var newdiv = document.createElement('div');
-          newdiv.innerHTML = "<label>Select</label> " + (counter + 1) + " <br><select class='form-control' name='idcollege' Required='true'><option value='math"+(counter + 1)+"'>Select College</option></select>";
+newdiv.innerHTML = "<label>Select</label> " + (counter + 1) + " <br><select class='form-control' name='idsubject' Required='true'><?php $query_college = mysqli_query($con,'SELECT * FROM subject'); if(mysqli_num_rows($query_college)>0){while($rows = mysqli_fetch_array($query_college)){$idsubject = $rows['idsubject'];$name = $rows['name'];?> <option value='<?php echo $name; ?>'> <?php echo $name; ?></option>  <?php }  }?></select>";
           document.getElementById(divName).appendChild(newdiv);
           counter++;
      }
